e8088f4f5c18e2e06cb2a9eb0078c19a86a95d00
ggzor <30713864+ggzor@users.noreply.github.com>
2021-12-06 13:28:48 -0600

    Adding day06 rust solution

---
+++ day06/Day06.rs
@@ -0,0 +1,42 @@
+#![feature(stdin_forwarders)]
+use std::collections::HashMap;
+use std::env;
+use std::io;
+
+fn main() {
+    let nums: Vec<u64> = io::stdin()
+        .lines()
+        .next()
+        .unwrap()
+        .unwrap()
+        .split(",")
+        .map(str::parse)
+        .map(Result::unwrap)
+        .collect();
+
+    let mut ls: HashMap<u64, u64> = Default::default();
+    for n in nums {
+        *ls.entry(n).or_default() += 1;
+    }
+
+    let top = if env::args().nth(1).unwrap() == "1" {
+        80
+    } else {
+        256
+    };
+
+    for _ in 1..=top {
+        let mut new: HashMap<u64, u64> = Default::default();
+        for (k, v) in &ls {
+            if *k == 0 {
+                *new.entry(6).or_default() += v;
+                *new.entry(8).or_default() += v;
+            } else {
+                *new.entry(k - 1).or_default() += v;
+            }
+        }
+        ls = new;
+    }
+
+    println!("{}", ls.values().sum::<u64>());
+}
