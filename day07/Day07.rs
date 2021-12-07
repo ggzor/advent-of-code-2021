0f0a09018d9f84fe2485b0bd50f01fb283222c9e
ggzor <30713864+ggzor@users.noreply.github.com>
2021-12-07 09:57:44 -0600

    Adding day07 Rust solution

---
+++ day07/Day07.rs
@@ -0,0 +1,29 @@
+#![feature(stdin_forwarders)]
+use std::convert::identity;
+use std::env;
+use std::io;
+
+fn main() {
+    let nums: Vec<i64> = io::stdin()
+        .lines()
+        .next()
+        .unwrap()
+        .unwrap()
+        .split(",")
+        .map(|s| s.parse().unwrap())
+        .collect();
+    let top = *nums.iter().max().unwrap();
+
+    let f: fn(i64) -> i64 = if env::args().nth(1).unwrap() == "1" {
+        identity
+    } else {
+        |x| x * (x + 1) / 2
+    };
+
+    let result: i64 = (0..=top)
+        .map(|i| nums.iter().map(|j| f((i - j).abs())).sum())
+        .min()
+        .unwrap();
+
+    println!("{}", result);
+}
