e1dc6a4902befde20fbc03b5ee21428b90eec084
ggzor <30713864+ggzor@users.noreply.github.com>
2021-12-12 13:34:05 -0600

    Cleaning up day12 Python solution

--- day12/Day12.py
+++ day12/Day12.py
@@ -1,78 +1,42 @@
 from sys import argv, stdin
-import itertools as it
-import operator as op
-from collections import Counter, defaultdict
+from collections import defaultdict
 
-conns = [*map(lambda s: s.strip().split("-"), stdin.readlines())]
-
-m = defaultdict(lambda: defaultdict(bool))
-
-for a, b in conns:
-    m[a][b] = True
-    m[b][a] = True
+g = {x for l in stdin if (v := tuple(l.strip().split("-"))) for x in [v, v[::-1]]}
 
 
-paths = []
+def dfs_one(cur, seen):
+    if cur in seen:
+        return 0
+    if cur == "end":
+        return 1
+
+    if cur.islower():
+        seen.add(cur)
+    r = sum(dfs_one(n, seen) for s, n in g if s == cur)
+    if cur.islower():
+        seen.remove(cur)
+    return r
 
 
-f = None
+def dfs_two(cur, start_seen, counters):
+    if (
+        (cur == "start" and start_seen)
+        or sum(c == 2 for c in counters.values()) > 1
+        or counters[cur] == 2
+    ):
+        return 0
+    if cur == "end":
+        return 1
+
+    if cur.islower():
+        counters[cur] += 1
+    r = sum(dfs_two(n, True, counters) for s, n in g if s == cur)
+    if cur.islower():
+        counters[cur] -= 1
+    return r
+
 
 if argv[1] == "1":
-
-    def dfs1(cur, seen, path):
-        if cur == "end":
-            paths.append(path.copy())
-
-        path.append(cur)
-
-        if cur.islower():
-            seen.add(cur)
-
-        for n, _ in m[cur].items():
-            if not n in seen:
-                dfs1(n, seen, path)
-
-        if cur.islower():
-            seen.remove(cur)
-        path = path[:-1]
-
-    f = dfs1
+    print(dfs_one("start", set()))
 else:
-    singles = defaultdict(int)
-
-    def dfs(cur, seen, path, depth=1):
-        global singles
-
-        if cur == "end":
-            paths.append(path.copy() + ["end"])
-            return
-
-        if cur.islower() and cur not in {"start", "end"}:
-            if singles[cur] < 1:
-                singles[cur] += 1
-            elif singles[cur] < 2 and all(v < 2 for v in singles.values()):
-                singles[cur] += 1
-            else:
-                return
-        elif cur.islower():
-            seen.add(cur)
-
-        path.append(cur)
-
-        for n, _ in m[cur].items():
-            if not n in seen:
-                dfs(n, seen, path, depth + 1)
-
-        if singles[cur] >= 1:
-            singles[cur] -= 1
-        elif cur.islower() and cur not in {"start", "end"}:
-            seen.remove(cur)
-
-        path.pop()
-
-    f = dfs
-
-
-f("start", set(), [])
-
-print(len(paths))
+    print(dfs_two("start", False, defaultdict(int)))
