1563011c5b477c94db03b503afa1f34874301f6c
ggzor <30713864+ggzor@users.noreply.github.com>
2022-01-04 17:09:57 -0600

    Cleaning up day23 Haskell solution

--- day23/Day23.hs
+++ day23/Day23.hs
@@ -1,33 +1,13 @@
-{-# LANGUAGE BlockArguments #-}
-{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE TupleSections #-}
-{-# LANGUAGE TypeApplications #-}
 
-import Control.Applicative
-import Control.Monad
+import Control.Monad.Reader
 import Data.Bifunctor
-import Data.Char as C
-import Data.Function
-import Data.Functor
-import qualified Data.IntMap.Strict as IM
 import qualified Data.List as L
 import qualified Data.Map.Strict as M
 import Data.Maybe
-import Data.Ord
 import qualified Data.Set as S
-import Data.Tuple
-import Numeric
-
-import Control.Monad.Reader
-import System.Environment
-import System.IO.Unsafe (unsafePerformIO)
-
-debug a = seq (unsafePerformIO $ print a)
-
-compDebug :: Show a => a -> a
-compDebug = debug <$> id <*> id
 
 {- Minimal priority queue implementation -}
 newtype PQueue k a = PQueue (M.Map k [a])
@@ -41,43 +21,24 @@ qsingleton k a = PQueue $ M.singleton k [a]
 qnull (PQueue m) = M.null m
 qinsert k a (PQueue m) = PQueue $ M.insertWith (++) k [a] m
 
+{- monad-loops would be helpful here -}
+spanM :: Monad m => (a -> m Bool) -> [a] -> m ([a], [a])
+spanM _ [] = pure ([], [])
+spanM p (x : xs) =
+  p x >>= \case
+    False -> pure ([], x : xs)
+    True -> first (x :) <$> spanM p xs
+
+takeWhileM :: Monad m => (a -> m Bool) -> [a] -> m [a]
+takeWhileM p xs = fst <$> spanM p xs
+
+allM p = foldM (\acc v -> (acc &&) <$> p v) True
+
 {- Solution -}
-
-temph = 5
-tempw = 13
-
-isSpaceB y x = y >= 3 && (x <= 1 || x >= tempw - 2)
-isBorderB y x =
-  y == 0 || y == 1 && (x == 0 || x == tempw - 1)
-    || y >= 2 && notElem x (M.elems target)
-    || y == temph - 1
-
-pprintBoard b =
-  unlines
-    [ [ if
-          | isSpaceB y x -> ' '
-          | isBorderB y x -> '#'
-          | otherwise -> fromMaybe '.' $ M.lookup (y, x) b
-      | x <- [0 .. tempw - 1]
-      ]
-    | y <- [0 .. temph - 1]
-    ]
-
-compShowBoard :: (Int, Board) -> (Int, Board)
-compShowBoard b@(cost, board) =
-  let action = do
-        print cost
-        putStrLn . pprintBoard $ board
-   in seq (unsafePerformIO action) b
-
 type Point = (Int, Int)
 type Movement = (Point, Point)
 type Board = M.Map Point Char
 type Diagram = (Int, Int, Board)
-
-moveCosts = M.fromList [('A', 1), ('B', 10), ('C', 100), ('D', 1000)]
-target = M.fromList [('A', 3), ('B', 5), ('C', 7), ('D', 9)]
-targetCols = S.fromList $ M.elems target
 
 parseBoard :: [String] -> Diagram
 parseBoard ls =
@@ -91,48 +52,72 @@ parseBoard ls =
       ]
   )
 
-isGoal = all (\((y, x), v) -> y >= 2 && x == target M.! v) . M.toList
+moveCosts = M.fromList [('A', 1), ('B', 10), ('C', 100), ('D', 1000)]
+target = M.fromList [('A', 3), ('B', 5), ('C', 7), ('D', 9)]
+targetCols = S.fromList $ M.elems target
+
+type DiagramM = Reader Diagram
+
+widthM :: DiagramM Int
+widthM = asks $ \(_, w, _) -> w
+
+boardM :: DiagramM Board
+boardM = asks $ \(_, _, b) -> b
 
 getOr def k m = fromMaybe def $ M.lookup k m
+getAt p = getOr '.' p <$> boardM
+isEmpty = fmap ('.' ==) . getAt
 
-nextMovements :: Diagram -> [(Int, Movement)]
-nextMovements d@(h, w, board) = concatMap checkPoint $ M.toList board
- where
-  checkPoint :: (Point, Char) -> [(Int, Movement)]
-  checkPoint p@((y, x), v) | y == 1 = filter (\(_, (_, (yi, _))) -> yi /= 1) $ bothSides d p
-  checkPoint p@((y, x), v)
-    | y >= 2
-        && all (\yi -> '.' == getOr '.' (yi, x) board) [y - 1, y - 2 .. 2]
-        && ( x /= target M.! v
-              || any (\yi -> v /= board M.! (yi, x)) [y + 1 .. h - 2]
-           ) =
-      map (first (+ (y - 1))) $ bothSides d p
-  checkPoint p = []
+nextMovements :: DiagramM [Movement]
+nextMovements =
+  boardM >>= fmap concat . mapM (\p -> fmap (p,) <$> movementsForPoint p) . M.keys
+
+movementsForPoint :: Point -> DiagramM [Point]
+movementsForPoint p@(y, x) = do
+  v <- getAt p
+  (h, w, b) <- ask
+  if
+      | y == 1 -> filter (\(yi, _) -> yi /= 1) <$> bothSidesFrom p
+      | y >= 2 -> do
+        isRoomStartEmpty <- allM isEmpty $ map (,x) [y - 1, y - 2 .. 2]
+        let mustMove = x /= target M.! v || any (\yi -> v /= b M.! (yi, x)) [y + 1 .. h - 2]
+        if isRoomStartEmpty && mustMove then bothSidesFrom p else pure []
+
+bothSidesFrom :: Point -> DiagramM [Point]
+bothSidesFrom p@(_, x) = do
+  w <- widthM
+  l <- oneSide p [x - 1, x - 2 .. 1]
+  r <- oneSide p [x + 1 .. w - 2]
+  pure $ l ++ r
+
+oneSide :: Point -> [Int] -> DiagramM [Point]
+oneSide p sideIter = do
+  v <- getAt p
+  (h, w, b) <- ask
+  hallway <- takeWhileM isEmpty $ map (1,) sideIter
+  let (sideRoomTops, validHallway) =
+        L.partition (\(_, x) -> S.member x targetCols) hallway
+  sideRoomPoint <- case L.find (\(_, x) -> x == target M.! v) hallway of
+    Nothing -> pure []
+    Just (_, xi) -> do
+      (start, end) <- spanM isEmpty $ map (,xi) [2 .. h - 2]
+      pure [last start | all ((v ==) . (b M.!)) end]
+  pure $ validHallway ++ sideRoomPoint
+
+applyMovement :: Board -> Movement -> Board
+applyMovement b (src, target) = M.insert target (b M.! src) $ M.delete src b
+
+distance (y1, x1) (y2, x2) = abs (x1 - x2) + (y1 - 1) + (y2 - 1)
 
 nextBoards :: Diagram -> [(Int, Board)]
 nextBoards d@(_, _, b) =
-  map (second (applyMovement b) . (\(c, m@(psrc, ptarget)) -> (c * moveCosts M.! (b M.! psrc), m))) $ nextMovements d
-
-applyMovement :: Board -> Movement -> Board
-applyMovement b (psrc, ptarget) = M.insert ptarget (b M.! psrc) $ M.delete psrc b
-
-bothSides :: Diagram -> (Point, Char) -> [(Int, Movement)]
-bothSides b@(_, w, _) p@((_, x), _) =
-  oneSide b p [x - 1, x - 2 .. 1] ++ oneSide b p [x + 1 .. w - 2]
-
-oneSide :: Diagram -> (Point, Char) -> [Int] -> [(Int, Movement)]
-oneSide d@(h, w, b) (psrc@(y, x), v) it =
-  let hallwayPoints = takeWhile (\p -> '.' == getOr '.' p b) . map (1,) $ it
-      (sideRoomTops, validHallway) = L.partition ((`S.member` targetCols) . snd) hallwayPoints
-      hallwayTargets = map (\p@(yi, xi) -> (abs $ x - xi, (psrc, p))) validHallway
-      sidePoint = L.find ((== target M.! v) . snd) sideRoomTops
-      sideRoomTarget = case sidePoint of
-        Nothing -> []
-        Just (_, xi) ->
-          let (initial, final) = L.span (\p -> '.' == getOr '.' p b) . map (,xi) $ [2 .. h - 2]
-              sideRoomPoint = [last initial | all (\p -> v == b M.! p) final]
-           in map (\p@(yi, xi) -> ((yi - 1) + abs (x - xi), (psrc, p))) sideRoomPoint
-   in hallwayTargets ++ sideRoomTarget
+  map
+    ( \m@(src, target) ->
+        ( distance src target * moveCosts M.! (b M.! src)
+        , applyMovement b m
+        )
+    )
+    $ runReader nextMovements d
 
 scanBoards :: Diagram -> [(Int, Board)]
 scanBoards (h, w, board) = go (qsingleton 0 board) S.empty
@@ -140,12 +125,16 @@ scanBoards (h, w, board) = go (qsingleton 0 board) S.empty
   go :: PQueue Int Board -> S.Set Board -> [(Int, Board)]
   go q _ | qnull q = []
   go q seen =
-    let (t@(dist, board), qnext) = deleteFindMin q
-        next =
+    let (r@(dist, board), qnext) = deleteFindMin q
+        new =
           if S.member board seen
             then []
-            else map (first (+ dist)) . filter ((`S.notMember` seen) . snd) $ nextBoards (h, w, board)
-     in t : go (L.foldl' (\q (c, b) -> qinsert c b q) qnext next) (S.insert board seen)
+            else
+              map (first (+ dist)) . filter ((`S.notMember` seen) . snd) $
+                nextBoards (h, w, board)
+     in r : go (L.foldl' (\q (c, b) -> qinsert c b q) qnext new) (S.insert board seen)
+
+isGoal = all (\((y, x), v) -> y >= 2 && x == target M.! v) . M.toList
 
 solve :: Diagram -> Int
 solve = maybe 0 fst . L.find (isGoal . snd) . scanBoards
